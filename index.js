38d90eef000a435686e7cb07c1e42d640494776d
Evgeniy <edubskiy@openwaygroup.com>
2020-09-15 16:10:22 +0300

    Added max, min extent calculations

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const svg = d3.select('svg');
 
 d3.json('menu.json').then(data => {
+  
+  const min = d3.min(data, d => d.orders);
+  const max = d3.max(data, d => d.orders);
+  const extent = d3.extent(data, d => d.orders);
 
   const y = d3.scaleLinear()
-    .domain([0, 1000])
+    .domain([0, max])
     .range([0, 500]);
+
 
   const x = d3.scaleBand()
     .domain(data.map(item => item.name))
@@ -23,7 +28,7 @@ d3.json('menu.json').then(data => {
   rects.enter()
     .append('rect')
       .attr('width', x.bandwidth)
-      .attr('height', d => d.orders)
+      .attr('height', d => y(d.orders))
       .attr('fill', 'orange')
       .attr('x', d => x(d.name));
 
